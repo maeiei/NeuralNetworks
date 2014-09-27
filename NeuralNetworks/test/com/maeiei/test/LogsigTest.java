f075793551fd2d858fa2d255ab06807e946a4c26
maeiei <maeiei@163.com>
2014-09-27 10:43:44 +0800

    `

--- NeuralNetworks/test/com/maeiei/test/LogsigTest.java
+++ NeuralNetworks/test/com/maeiei/test/LogsigTest.java
@@ -1,7 +1,9 @@
 package com.maeiei.test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.Is.*;
+import static org.hamcrest.core.IsEqual.*;
+
 import java.text.DecimalFormat;
 
 import org.junit.Test;
@@ -12,16 +14,17 @@ import com.maeiei.Matrix;
 
 public class LogsigTest {
 	@Test
-	public void testfunction() {
+	public void testLogsig() {
 		double[][] matrixData = { { -0.75, -0.54 } };
+		double[][] expectedData = { { 0.320821300824607, 0.3681875822638983 } };
 		Matrix input = new Matrix(matrixData);
 
 		Function function = new Logsig();
 		Matrix actual = function.function(input);
-		assertThat("", matrixData[0][0], is(matrixData[0][1]));
-		// assertThat("乘积的行数为", actual.get(0, 1), is(0.368d));
-		// assertThat("乘积的行数为", s, is("0.321"));
-		// assertThat("乘积的行数为", actual.get(0, 1), is(0.368));
+		assertThat("乘积的行数为", actual.get(0, 1), is(expectedData[0][1]));
+		DecimalFormat format = new DecimalFormat("#.000");
+		assertThat("乘积的行数为", format.format(actual.get(0, 0)), equalTo(".321"));
+		assertThat("乘积的行数为", format.format(actual.get(0, 1)), equalTo(".368"));
 		System.out.println(actual);
 	}
 }
